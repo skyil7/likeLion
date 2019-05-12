37a07ea7f3141a5dbe4fdec404b30c06415ae0e6
skyil7 <nakagimatv.2@gmail.com>
2019-05-12 20:16:29 +0900

    commit

--- Django/FirstProject/FirstProject/urls.py
+++ Django/FirstProject/FirstProject/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name="home"),
     path('about/', wordcount.views.about, name="about"),
+    path('result/', wordcount.views.result, name="result"),
 ]
--- Django/FirstProject/wordcount/views.py
+++ Django/FirstProject/wordcount/views.py
@@ -7,3 +7,17 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split(' ')
+    wordQuantity = len(words)
+    wordDictionary = {}
+
+    for word in words:
+        if word in wordDictionary:
+            wordDictionary[word]+=1
+        else:
+            wordDictionary[word]=1
+
+    return render(request, 'result.html', {'fulltext':text, "words":words, "wq":wordQuantity, "wordCount":wordDictionary.items()}) #text를 담어 같이 전송
