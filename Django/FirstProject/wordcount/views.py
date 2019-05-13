7371c29bc05ceb628fcba54d1f29fc9d8c11aaa9
skyil7 <giopaik@naver.com>
2019-05-13 19:02:29 +0900

    commit

--- Django/FirstProject/wordcount/views.py
+++ Django/FirstProject/wordcount/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import operator
+import re#정규표현식
 
 # Create your views here.
 
@@ -11,9 +12,11 @@ def about(request):
 
 def result(request):
     text = request.GET['fulltext']
-    words = text.split(' ')
+    words = re.split("\W",text)#특수문자 전체 기준으로 split
     wordQuantity = len(words)
     wordDictionary = {}
+    textLength = len(text)
+    smallLength = len(text)-wordQuantity+1
 
     while '' in words:
         words.remove('')
@@ -25,4 +28,4 @@ def result(request):
             wordDictionary[word]=1
 
     wordCount = sorted(wordDictionary.items(), key=operator.itemgetter(1), reverse=True)#나온 횟수에 따라 정렬
-    return render(request, 'result.html', {'fulltext':text, "words":words, "wq":wordQuantity, "wordCount":wordCount}) #text를 담어 같이 전송
+    return render(request, 'result.html', {'fulltext':text, "words":words, "wq":wordQuantity, "wordCount":wordCount, "textLength":textLength,"smallLength":smallLength}) #text를 담어 같이 전송
